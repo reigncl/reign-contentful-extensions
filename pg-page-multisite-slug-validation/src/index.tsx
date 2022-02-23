445a84c20c841739d338b295f311490861ecb7cb
Jaime Vasquez <rodrigoxv@gmail.com>
2022-02-23 15:41:26 -0300

    feat: pg-page-multisite-slug-validation

--- pg-page-multisite-slug-validation/src/index.tsx
+++ pg-page-multisite-slug-validation/src/index.tsx
@@ -1,71 +1,120 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
-import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import {render} from 'react-dom';
+import { TextInput, ValidationMessage } from '@contentful/forma-36-react-components';
+import {init, FieldExtensionSDK, SearchQuery } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
+import {useEffect, useState} from "react";
 
 interface AppProps {
-  sdk: FieldExtensionSDK;
+   sdk: FieldExtensionSDK;
 }
 
-interface AppState {
-  value?: string;
-}
+const App = (props: AppProps) => {
+   const [value, setValue] = useState<string>(props.sdk.field?.getValue() ? props.sdk.field.getValue() : '')
+   const [site, setSite] = useState<string>(props.sdk.entry?.fields['site']?.getValue() || '');
+   const [fieldValue, setFieldValue] = useState<string>(props.sdk.field?.getValue() ? props.sdk.field.getValue() : '');
+   const [isSlugUsed, setIsSlugUsed] = useState<boolean>(false);
 
-export class App extends React.Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      value: props.sdk.field.getValue() || ''
-    };
-  }
+   let detachExternalChangeHandler: Function | null = null;
+   let detachSiteChangeHandler: Function | null = null;
 
-  detachExternalChangeHandler: Function | null = null;
+   useEffect(() => {
+      props.sdk.window.startAutoResizer();
+      detachSiteChangeHandler = props.sdk.entry?.fields['site'].onValueChanged(siteChangeHandler);
+      detachExternalChangeHandler = props.sdk.field?.onValueChanged(onExternalChange);
 
-  componentDidMount() {
-    this.props.sdk.window.startAutoResizer();
+      return () => {
+         if (detachSiteChangeHandler) detachSiteChangeHandler();
+         if (detachExternalChangeHandler) detachExternalChangeHandler();
+      }
+   }, []);
 
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
-  }
+   useEffect(() => {
+      const timeout = setTimeout(() => {
+         validateSlugUsed().then(result => {
+            setIsSlugUsed(result);
+            if (result) {
+               setFieldValue('');
+            } else {
+               setFieldValue(value);
+            }
+         })
+      }, 1500)
 
-  componentWillUnmount() {
-    if (this.detachExternalChangeHandler) {
-      this.detachExternalChangeHandler();
-    }
-  }
 
-  onExternalChange = (value: string) => {
-    this.setState({ value });
-  };
+      return () => clearTimeout(timeout)
+   }, [value]);
 
-  onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    this.setState({ value });
-    if (value) {
-      await this.props.sdk.field.setValue(value);
-    } else {
-      await this.props.sdk.field.removeValue();
-    }
-  };
+   useEffect(() => {
+      if (fieldValue) {
+         props.sdk.field?.setValue(fieldValue).then();
+      } else {
+         props.sdk.field?.removeValue().then();
+      }
+   }, [fieldValue])
 
-  render() {
-    return (
-      <TextInput
-        width="large"
-        type="text"
-        id="my-field"
-        testId="my-field"
-        value={this.state.value}
-        onChange={this.onChange}
-      />
-    );
-  };
+   useEffect(() => {
+      validateSlugUsed().then(result => {
+         setIsSlugUsed(result);
+         if (result) {
+            setFieldValue('');
+         } else {
+            setFieldValue(value);
+         }
+      })
+   }, [site])
+
+   const onExternalChange = (externalValue: string) => {
+      if (externalValue) {
+         setValue(externalValue);
+      }
+   };
+
+   const siteChangeHandler = (site: string) => {
+      setSite(site);
+   }
+
+   const validateSlugUsed = async () => {
+      if (value && site) {
+         const thisId = props.sdk.entry.getSys().id;
+         const searchQuery: SearchQuery = {
+            limit: 5,
+            ['content_type']: 'pg-page',
+            ['fields.site[in]']: site,
+            ['fields.slug[in]']: value,
+            ['sys.id[nin]']: thisId,
+         }
+         const result = await props.sdk.space.getEntries(searchQuery);
+         if (result.total > 0) {
+            setIsSlugUsed(true);
+            return true;
+         }
+      }
+      return false;
+   }
+
+   const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.currentTarget.value;
+      setValue(value);
+   };
+
+
+   return (
+       <>
+          <TextInput
+              value={value}
+              onChange={event => onChange(event as React.ChangeEvent<HTMLInputElement>)}
+          />
+          {isSlugUsed && <ValidationMessage style={{marginTop: '0.5rem'}}>
+              El slug ya se encuentra en uso para el sitio seleccionado
+          </ValidationMessage>}
+       </>
+   );
 }
 
 init(sdk => {
-  render(<App sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+   render(<App sdk={sdk as FieldExtensionSDK}/>, document.getElementById('root'));
 });
 
 /**
