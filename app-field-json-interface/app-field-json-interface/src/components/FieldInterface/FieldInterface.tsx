72f5723e49bf41acdfc0d9e332c7b3fe24fe771c
Jaime Vasquez <rodrigoxv@gmail.com>
2024-01-09 21:24:13 -0300

    support array

--- app-field-json-interface/app-field-json-interface/src/components/FieldInterface/FieldInterface.tsx
+++ app-field-json-interface/app-field-json-interface/src/components/FieldInterface/FieldInterface.tsx
@@ -9,24 +9,31 @@ import { useEffect, useState } from "react";
 import { AppInstallationParameters } from "../../locations/ConfigScreen";
 import { CollectionProp, EntryProps, KeyValueMap } from "contentful-management";
 import EditorsHandler from "./Editors";
+import {
+  Flex,
+  Button,
+  Grid,
+  IconButton,
+  Badge,
+} from "@contentful/f36-components";
+import tokens from "@contentful/f36-tokens";
+import { FieldInterfaceValue } from "./FieldInterface.types";
+import { DeleteIcon } from "@contentful/f36-icons";
 
 export interface FieldSetupProps {
   sdk: FieldAppSDK;
 }
 
 const FieldInterface = ({ sdk }: FieldSetupProps) => {
-  const [value, setValue] = useState<Record<string, unknown>>(
+  const [value, setValue] = useState<FieldInterfaceValue>(
     (sdk.field.getValue() as Record<string, unknown>) ?? {}
   );
   const parameters = sdk.parameters.installation as AppInstallationParameters;
   const [interfaceField, setInterfaceField] = useState<Interface | undefined>(
     undefined
   );
-  const [configurationField, setConfigurationField] = useState<
-    FieldSetupItem | undefined
-  >(undefined);
 
-  const handleUpdate = async (value: Record<string, unknown>) => {
+  const handleUpdate = async (value: FieldInterfaceValue) => {
     await sdk.field.setValue(value);
     setValue(value);
     setInterfaceField({ ...(interfaceField as Interface) });
@@ -56,12 +63,80 @@ const FieldInterface = ({ sdk }: FieldSetupProps) => {
           )?.interfaces?.find(
             (inter: Interface) => inter.id === findConfig?.interfaceId
           );
-          setConfigurationField(findConfig);
           setInterfaceField(findInterface);
         }
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sdk]);
+
+  if (interfaceField?.isArray) {
+    const arrValue = Array.isArray(value) ? value : [];
+    return (
+      <>
+        {arrValue?.map((val: Record<string, unknown>, index: number) => (
+          <Grid
+            key={`grid-${index}`}
+            style={{
+              width: "100%",
+              borderBottom: "1px solid rgb(207, 217, 224)",
+              paddingTop: tokens.spacingS,
+            }}
+            columns="0.8fr 6fr 0.3fr"
+            rowGap="spacingM"
+            columnGap="spacingM"
+          >
+            <Grid.Item>
+              <Badge>{index + 1}</Badge>
+            </Grid.Item>
+            <Grid.Item>
+              {interfaceField?.items?.map(
+                (item: InterfaceItem, idx: number) => (
+                  <EditorsHandler
+                    key={`EditorsHandler-${idx}`}
+                    interfaceItem={item}
+                    updateValue={(update: Record<string, unknown>) => {
+                      arrValue[index] = update;
+                      handleUpdate(arrValue);
+                    }}
+                    value={val as Record<string, unknown>}
+                  />
+                )
+              )}
+            </Grid.Item>
+            <Grid.Item>
+              <IconButton
+                variant="transparent"
+                aria-label="Remove Item"
+                icon={<DeleteIcon />}
+                onClick={async () => {
+                  const response = await sdk.dialogs.openConfirm({
+                    title: "Remove Item",
+                    message: "Are you sure you want to delete this item?",
+                  });
+                  if (response) {
+                    arrValue.splice(index, 1);
+                    handleUpdate(arrValue);
+                  }
+                }}
+              />
+            </Grid.Item>
+          </Grid>
+        ))}
+        <Flex padding="spacingS" justifyContent="right">
+          <Button
+            onClick={async () => {
+              arrValue.push({});
+              handleUpdate(arrValue);
+            }}
+            variant="primary"
+            size="small"
+          >
+            Add item
+          </Button>
+        </Flex>
+      </>
+    );
+  }
 
   return (
     <>
@@ -69,7 +144,7 @@ const FieldInterface = ({ sdk }: FieldSetupProps) => {
         <EditorsHandler
           interfaceItem={item}
           updateValue={handleUpdate}
-          value={value}
+          value={value as Record<string, unknown>}
         />
       ))}
     </>
---
+++ app-field-json-interface/app-field-json-interface/src/components/FieldInterface/FieldInterface.types.ts
@@ -0,0 +1,3 @@
+export type FieldInterfaceValue =
+  | Record<string, unknown>
+  | Array<Record<string, unknown>>;
