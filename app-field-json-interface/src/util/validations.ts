22dd848e61cd7708e598e8476c09b59e992bf55e
Jaime Vasquez <rodrigoxv@gmail.com>
2024-01-29 13:26:43 -0300

    Update validations.ts

--- app-field-json-interface/src/util/validations.ts
+++ app-field-json-interface/src/util/validations.ts
@@ -21,8 +21,7 @@ const validateField = (
         regexString = /^[0-9]*$/;
         break;
       case "url":
-        regexString =
-          /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+        regexString = /^((https?:\/\/(www\.)?[a-zA-Z0-9@:%._+~#=]{2,256})\.[a-z]{2,6}\b([A-Za-z0-9@:%_+.~#?&/=-]*))|(\/([A-Za-z0-9-]+(\/[A-Za-z0-9-]+)*)*)$/;
         break;
       case "regex":
         regexString = item?.regex;
